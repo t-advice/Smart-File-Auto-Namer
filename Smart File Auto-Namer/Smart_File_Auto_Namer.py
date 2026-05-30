fdf49169eff8dc80e005e092aa3840d3a6f31d2f
Tashwill <tashwilladvice02@gmail.com>
2026-05-30 21:15:32 +0200

    #3. Create a unique filename for recording base_filename = f"audio_record_{timestamp}.wav"

--- Smart File Auto-Namer/Smart_File_Auto_Namer.py
+++ Smart File Auto-Namer/Smart_File_Auto_Namer.py
@@ -12,3 +12,10 @@ timestamp = now.strftime("%Y-%m-%d %H-%M-%S")
 print(f"Current Timestamp: {timestamp}")
 
 #3. Create a unique filename for recording
+base_filename = f"audio_record_{timestamp}.wav"
+print(f"Generated Secure Filename:{base_filename}")
+print("-" *36)
+
+
+
+
