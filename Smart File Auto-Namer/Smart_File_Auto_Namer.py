0e92982581d8d43b0288c7c3915027893d31cc22
Tashwill <tashwilladvice02@gmail.com>
2026-05-30 21:13:22 +0200

    #2. Formating the into a text ( string) timestamp = now.strftime("%Y-%m-%d %H-%M-%S") print(f"Current Timestamp: {timestamp}")

--- Smart File Auto-Namer/Smart_File_Auto_Namer.py
+++ Smart File Auto-Namer/Smart_File_Auto_Namer.py
@@ -1,1 +1,14 @@
 from datetime import datetime # Clock Library
+import os
+
+print("TASHWILL's Smart File Auto-Namer")
+print("-" *36)
+
+#1. Get the current date and time from my laptop 
+now = datetime.now()
+
+#2. Formating the into a text ( string)
+timestamp = now.strftime("%Y-%m-%d %H-%M-%S")
+print(f"Current Timestamp: {timestamp}")
+
+#3. Create a unique filename for recording
